ffdcda9dd3c938b2160c8a3756391f008847041e
Andrei TV <a.talviy@gmail.com>
2015-01-10 19:31:57 +0100

    xUnit test project for future migration

--- HackerCup.Test/2011.cs
+++ HackerCup.Test/2011.cs
@@ -6,299 +6,257 @@ using HackerCup._2011.R1C;
 using HackerCup._2011.R2;
 using HackerCup._2011.R3;
 using HackerCup._2011.Test;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Xunit;
 
 namespace HackerCup.Test
 {
-    [TestClass]
     public class _2011
     {
-        [TestMethod]
-        public void _DoubleSquares()
+        [Theory]
+        [InlineData("10", "1")]
+        [InlineData("25", "2")]
+        [InlineData("3", "0")]
+        [InlineData("0", "1")]
+        [InlineData("1", "1")]
+        public void Should_return_double_squares_when_execute_input(string input, string expectedResult)
         {
-            DoubleSquares ds = new DoubleSquares();
+            var tested = new DoubleSquares();
 
-            Helper.CustomAreEqual("1", ds.Execute("10"));
-            Helper.CustomAreEqual("2", ds.Execute("25"));
-            Helper.CustomAreEqual("0", ds.Execute("3"));
-            Helper.CustomAreEqual("1", ds.Execute("0"));
-            Helper.CustomAreEqual("1", ds.Execute("1"));
+            Assert.Equal(expectedResult, tested.Execute(input)[0]);
         }
 
-        [TestMethod]
-        public void _DoubleSquares_large()
+        //[Fact]
+        //public void _DoubleSquares_large()
+        //{
+        //    Helper.RunLarge<DoubleSquares>("Assets/double_squares.txt", 1);
+        //}
+
+        [Theory]
+        [InlineData("5 4 0 1 2 2", "0 0.375000")]
+        [InlineData("3 4 1 1 1 1", "XXX")]
+        [InlineData("3 3 1 2 1 1 1 0", "1 1.000000")]
+        [InlineData("3 4 0 2 1 0 1 1", "0 1.000000")]
+        [InlineData("3 4 0 1 1 1", "0 0.500000")]
+        public void Should_return_peg_game_when_execute_input(string input, string expectedResult)
         {
-            Helper.RunLarge<DoubleSquares>("Assets/double_squares.txt", 1);
+            var tested = new PegGame();
+
+            Assert.Equal(expectedResult, tested.Execute(input)[0]);
         }
 
-        [TestMethod]
-        public void _PegGame()
+        //[TestMethod]
+        //public void _PegGame_large()
+        //{
+        //    Helper.RunLarge<DoubleSquares>("Assets/peg_game.txt", 1);
+        //}
+
+        [Theory]
+        [InlineData("6 facebook hacker cup for studious students", "cupfacebookforhackerstudentsstudious")]
+        [InlineData("5 k duz q rc lvraw", "duzklvrawqrc")]
+        [InlineData("5 mybea zdr yubx xe dyroiy", "dyroiymybeaxeyubxzdr")]
+        [InlineData("5 jibw ji jp bw jibw", "bwjibwjibwjijp")]
+        [InlineData("5 uiuy hopji li j dcyi", "dcyihopjijliuiuy")]
+        public void Should_return_studious_student_when_execute_input(string input, string expectedResult)
         {
-            PegGame ds = new PegGame();
+            var tested = new StudiousStudent();
 
-            Helper.CustomAreEqual("0 0.375000", ds.Execute("5 4 0 1 2 2"));
-            Helper.CustomAreEqual("XXX", ds.Execute("3 4 1 1 1 1"));
-            Helper.CustomAreEqual("1 1.000000", ds.Execute("3 3 1 2 1 1 1 0"));
-            Helper.CustomAreEqual("0 1.000000", ds.Execute("3 4 0 2 1 0 1 1"));
-            Helper.CustomAreEqual("0 0.500000", ds.Execute("3 4 0 1 1 1"));
-        }
-
-        [TestMethod]
-        public void _PegGame_large()
-        {
-            Helper.RunLarge<DoubleSquares>("Assets/peg_game.txt", 1);
-        }
-
-        [TestMethod]
-        public void _StudiousStudent()
-        {
-            StudiousStudent ds = new StudiousStudent();
-
-            Helper.CustomAreEqual("cupfacebookforhackerstudentsstudious", ds.Execute("6 facebook hacker cup for studious students"));
-            Helper.CustomAreEqual("duzklvrawqrc", ds.Execute("5 k duz q rc lvraw"));
-            Helper.CustomAreEqual("dyroiymybeaxeyubxzdr", ds.Execute("5 mybea zdr yubx xe dyroiy"));
-            Helper.CustomAreEqual("bwjibwjibwjijp", ds.Execute("5 jibw ji jp bw jibw"));
-            Helper.CustomAreEqual("dcyihopjijliuiuy", ds.Execute("5 uiuy hopji li j dcyi"));
+            Assert.Equal(expectedResult, tested.Execute(input)[0]);
         }
 
 
-        [TestMethod]
-        public void _StudiousStudent_large()
+        //[Fact]
+        //public void _StudiousStudent_large()
+        //{
+        //    Helper.RunLarge<StudiousStudent>("Assets/studious_student.txt", 1);
+        //}
+
+        [Theory]
+        [InlineData("XX", "XX")]
+        public void _AfterTheDanceBattle(string input, string expectedResult)
         {
-            Helper.RunLarge<StudiousStudent>("Assets/studious_student.txt", 1);
+            var tested = new AfterTheDanceBattle();
+
+            Assert.Equal(expectedResult, tested.Execute(input)[0]);
         }
 
-        [TestMethod]
-        public void _AfterTheDanceBattle()
+        [Theory]
+        [InlineData("XX", "XX")]
+        public void _FirstOrLast(string input, string expectedResult)
         {
+            var tested = new FirstOrLast();
 
-            var t = new AfterTheDanceBattle();
-
-            Helper.CustomAreEqual("XX", t.Execute("XX"));
-            Helper.CustomAreEqual("XX", t.Execute("XX"));
-            Helper.CustomAreEqual("XX", t.Execute("XX"));
-            Helper.CustomAreEqual("XX", t.Execute("XX"));
-            Helper.CustomAreEqual("XX", t.Execute("XX"));
+            Assert.Equal(expectedResult, tested.Execute(input)[0]);
         }
-        [TestMethod]
-        public void _FirstOrLast()
-        {
-            var t = new FirstOrLast();
 
-            Helper.CustomAreEqual("XX", t.Execute("XX"));
-            Helper.CustomAreEqual("XX", t.Execute("XX"));
-            Helper.CustomAreEqual("XX", t.Execute("XX"));
-            Helper.CustomAreEqual("XX", t.Execute("XX"));
-            Helper.CustomAreEqual("XX", t.Execute("XX"));
+        [Theory]
+        [InlineData("XX", "XX")]
+        public void _PowerOverwhelming(string input, string expectedResult)
+        {
+            var tested = new PowerOverwhelming();
+
+            Assert.Equal(expectedResult, tested.Execute(input)[0]);
         }
-        [TestMethod]
-        public void _PowerOverwhelming()
-        {
-            var t = new PowerOverwhelming();
 
-            Helper.CustomAreEqual("XX", t.Execute("XX"));
-            Helper.CustomAreEqual("XX", t.Execute("XX"));
-            Helper.CustomAreEqual("XX", t.Execute("XX"));
-            Helper.CustomAreEqual("XX", t.Execute("XX"));
-            Helper.CustomAreEqual("XX", t.Execute("XX"));
+        [Theory]
+        [InlineData("XX", "XX")]
+        public void _DiversityNumber(string input, string expectedResult)
+        {
+            var tested = new DiversityNumber();
+
+            Assert.Equal(expectedResult, tested.Execute(input)[0]);
         }
-        [TestMethod]
-        public void _DiversityNumber()
-        {
-            var t = new DiversityNumber();
 
-            Helper.CustomAreEqual("XX", t.Execute("XX"));
-            Helper.CustomAreEqual("XX", t.Execute("XX"));
-            Helper.CustomAreEqual("XX", t.Execute("XX"));
-            Helper.CustomAreEqual("XX", t.Execute("XX"));
-            Helper.CustomAreEqual("XX", t.Execute("XX"));
+        [Theory]
+        [InlineData("XX", "XX")]
+        public void _TurnOnTheLights(string input, string expectedResult)
+        {
+            var tested = new TurnOnTheLights();
+
+            Assert.Equal(expectedResult, tested.Execute(input)[0]);
         }
-        [TestMethod]
-        public void _TurnOnTheLights()
-        {
-            var t = new TurnOnTheLights();
 
-            Helper.CustomAreEqual("XX", t.Execute("XX"));
-            Helper.CustomAreEqual("XX", t.Execute("XX"));
-            Helper.CustomAreEqual("XX", t.Execute("XX"));
-            Helper.CustomAreEqual("XX", t.Execute("XX"));
-            Helper.CustomAreEqual("XX", t.Execute("XX"));
+        [Theory]
+        [InlineData("XX", "XX")]
+        public void _WineTasting(string input, string expectedResult)
+        {
+            var tested = new WineTasting();
+
+            Assert.Equal(expectedResult, tested.Execute(input)[0]);
         }
-        [TestMethod]
-        public void _WineTasting()
-        {
-            var t = new WineTasting();
 
-            Helper.CustomAreEqual("XX", t.Execute("XX"));
-            Helper.CustomAreEqual("XX", t.Execute("XX"));
-            Helper.CustomAreEqual("XX", t.Execute("XX"));
-            Helper.CustomAreEqual("XX", t.Execute("XX"));
-            Helper.CustomAreEqual("XX", t.Execute("XX"));
+        [Theory]
+        [InlineData("XX", "XX")]
+        public void _Chess2(string input, string expectedResult)
+        {
+            var tested = new Chess2();
+
+            Assert.Equal(expectedResult, tested.Execute(input)[0]);
         }
-        [TestMethod]
-        public void _Chess2()
-        {
-            var t = new Chess2();
 
-            Helper.CustomAreEqual("XX", t.Execute("XX"));
-            Helper.CustomAreEqual("XX", t.Execute("XX"));
-            Helper.CustomAreEqual("XX", t.Execute("XX"));
-            Helper.CustomAreEqual("XX", t.Execute("XX"));
-            Helper.CustomAreEqual("XX", t.Execute("XX"));
+        [Theory]
+        [InlineData("XX", "XX")]
+        public void _DiminishingCircle(string input, string expectedResult)
+        {
+            var tested = new DiminishingCircle();
+
+            Assert.Equal(expectedResult, tested.Execute(input)[0]);
         }
-        [TestMethod]
-        public void _DiminishingCircle()
-        {
-            var t = new DiminishingCircle();
 
-            Helper.CustomAreEqual("XX", t.Execute("XX"));
-            Helper.CustomAreEqual("XX", t.Execute("XX"));
-            Helper.CustomAreEqual("XX", t.Execute("XX"));
-            Helper.CustomAreEqual("XX", t.Execute("XX"));
-            Helper.CustomAreEqual("XX", t.Execute("XX"));
+        [Theory]
+        [InlineData("XX", "XX")]
+        public void _SlotMachineHacker(string input, string expectedResult)
+        {
+            var tested = new SlotMachineHacker();
+
+            Assert.Equal(expectedResult, tested.Execute(input)[0]);
         }
-        [TestMethod]
-        public void _SlotMachineHacker()
-        {
-            var t = new SlotMachineHacker();
 
-            Helper.CustomAreEqual("XX", t.Execute("XX"));
-            Helper.CustomAreEqual("XX", t.Execute("XX"));
-            Helper.CustomAreEqual("XX", t.Execute("XX"));
-            Helper.CustomAreEqual("XX", t.Execute("XX"));
-            Helper.CustomAreEqual("XX", t.Execute("XX"));
+        [Theory]
+        [InlineData("XX", "XX")]
+        public void _NFactorful(string input, string expectedResult)
+        {
+            var tested = new NFactorful();
+
+            Assert.Equal(expectedResult, tested.Execute(input)[0]);
         }
-        [TestMethod]
-        public void _NFactorful()
-        {
-            var t = new NFactorful();
 
-            Helper.CustomAreEqual("XX", t.Execute("XX"));
-            Helper.CustomAreEqual("XX", t.Execute("XX"));
-            Helper.CustomAreEqual("XX", t.Execute("XX"));
-            Helper.CustomAreEqual("XX", t.Execute("XX"));
-            Helper.CustomAreEqual("XX", t.Execute("XX"));
+        [Theory]
+        [InlineData("XX", "XX")]
+        public void _PolynomialFactoring(string input, string expectedResult)
+        {
+            var tested = new PolynomialFactoring();
+
+            Assert.Equal(expectedResult, tested.Execute(input)[0]);
         }
-        [TestMethod]
-        public void _PolynomialFactoring()
-        {
-            var t = new PolynomialFactoring();
 
-            Helper.CustomAreEqual("XX", t.Execute("XX"));
-            Helper.CustomAreEqual("XX", t.Execute("XX"));
-            Helper.CustomAreEqual("XX", t.Execute("XX"));
-            Helper.CustomAreEqual("XX", t.Execute("XX"));
-            Helper.CustomAreEqual("XX", t.Execute("XX"));
+        [Theory]
+        [InlineData("XX", "XX")]
+        public void _RiskySlide(string input, string expectedResult)
+        {
+            var tested = new RiskySlide();
+
+            Assert.Equal(expectedResult, tested.Execute(input)[0]);
         }
-        [TestMethod]
-        public void _RiskySlide()
-        {
-            var t = new RiskySlide();
 
-            Helper.CustomAreEqual("XX", t.Execute("XX"));
-            Helper.CustomAreEqual("XX", t.Execute("XX"));
-            Helper.CustomAreEqual("XX", t.Execute("XX"));
-            Helper.CustomAreEqual("XX", t.Execute("XX"));
-            Helper.CustomAreEqual("XX", t.Execute("XX"));
+        [Theory]
+        [InlineData("XX", "XX")]
+        public void _BonusAssignments(string input, string expectedResult)
+        {
+            var tested = new BonusAssignments();
+
+            Assert.Equal(expectedResult, tested.Execute(input)[0]);
         }
-        [TestMethod]
-        public void _BonusAssignments()
-        {
-            var t = new BonusAssignments();
 
-            Helper.CustomAreEqual("XX", t.Execute("XX"));
-            Helper.CustomAreEqual("XX", t.Execute("XX"));
-            Helper.CustomAreEqual("XX", t.Execute("XX"));
-            Helper.CustomAreEqual("XX", t.Execute("XX"));
-            Helper.CustomAreEqual("XX", t.Execute("XX"));
+        [Theory]
+        [InlineData("XX", "XX")]
+        public void _ScottsNewTrick(string input, string expectedResult)
+        {
+            var tested = new ScottsNewTrick();
+
+            Assert.Equal(expectedResult, tested.Execute(input)[0]);
         }
-        [TestMethod]
-        public void _ScottsNewTrick()
-        {
-            var t = new ScottsNewTrick();
 
-            Helper.CustomAreEqual("XX", t.Execute("XX"));
-            Helper.CustomAreEqual("XX", t.Execute("XX"));
-            Helper.CustomAreEqual("XX", t.Execute("XX"));
-            Helper.CustomAreEqual("XX", t.Execute("XX"));
-            Helper.CustomAreEqual("XX", t.Execute("XX"));
+        [Theory]
+        [InlineData("XX", "XX")]
+        public void _StudiousStudentII(string input, string expectedResult)
+        {
+            var tested = new StudiousStudentII();
+
+            Assert.Equal(expectedResult, tested.Execute(input)[0]);
         }
-        [TestMethod]
-        public void _StudiousStudentII()
-        {
-            var t = new StudiousStudentII();
 
-            Helper.CustomAreEqual("XX", t.Execute("XX"));
-            Helper.CustomAreEqual("XX", t.Execute("XX"));
-            Helper.CustomAreEqual("XX", t.Execute("XX"));
-            Helper.CustomAreEqual("XX", t.Execute("XX"));
-            Helper.CustomAreEqual("XX", t.Execute("XX"));
+        [Theory]
+        [InlineData("XX", "XX")]
+        public void _AlienGame(string input, string expectedResult)
+        {
+            var tested = new AlienGame();
+
+            Assert.Equal(expectedResult, tested.Execute(input)[0]);
         }
-        [TestMethod]
-        public void _AlienGame()
-        {
-            var t = new AlienGame();
 
-            Helper.CustomAreEqual("XX", t.Execute("XX"));
-            Helper.CustomAreEqual("XX", t.Execute("XX"));
-            Helper.CustomAreEqual("XX", t.Execute("XX"));
-            Helper.CustomAreEqual("XX", t.Execute("XX"));
-            Helper.CustomAreEqual("XX", t.Execute("XX"));
+        [Theory]
+        [InlineData("XX", "XX")]
+        public void _PartyTime(string input, string expectedResult)
+        {
+            var tested = new PartyTime();
+
+            Assert.Equal(expectedResult, tested.Execute(input)[0]);
         }
-        [TestMethod]
-        public void _PartyTime()
-        {
-            var t = new PartyTime();
 
-            Helper.CustomAreEqual("XX", t.Execute("XX"));
-            Helper.CustomAreEqual("XX", t.Execute("XX"));
-            Helper.CustomAreEqual("XX", t.Execute("XX"));
-            Helper.CustomAreEqual("XX", t.Execute("XX"));
-            Helper.CustomAreEqual("XX", t.Execute("XX"));
+        [Theory]
+        [InlineData("XX", "XX")]
+        public void _SafestPlace(string input, string expectedResult)
+        {
+            var tested = new SafestPlace();
+
+            Assert.Equal(expectedResult, tested.Execute(input)[0]);
         }
-        [TestMethod]
-        public void _SafestPlace()
-        {
-            var t = new SafestPlace();
 
-            Helper.CustomAreEqual("XX", t.Execute("XX"));
-            Helper.CustomAreEqual("XX", t.Execute("XX"));
-            Helper.CustomAreEqual("XX", t.Execute("XX"));
-            Helper.CustomAreEqual("XX", t.Execute("XX"));
-            Helper.CustomAreEqual("XX", t.Execute("XX"));
+        [Theory]
+        [InlineData("XX", "XX")]
+        public void _Almost(string input, string expectedResult)
+        {
+            var tested = new Almost();
+
+            Assert.Equal(expectedResult, tested.Execute(input)[0]);
         }
-        [TestMethod]
-        public void _Almost()
-        {
-            var t = new Almost();
 
-            Helper.CustomAreEqual("XX", t.Execute("XX"));
-            Helper.CustomAreEqual("XX", t.Execute("XX"));
-            Helper.CustomAreEqual("XX", t.Execute("XX"));
-            Helper.CustomAreEqual("XX", t.Execute("XX"));
-            Helper.CustomAreEqual("XX", t.Execute("XX"));
+        [Theory]
+        [InlineData("XX", "XX")]
+        public void _Characters(string input, string expectedResult)
+        {
+            var tested = new Characters();
+
+            Assert.Equal(expectedResult, tested.Execute(input)[0]);
         }
-        [TestMethod]
-        public void _Characters()
-        {
-            var t = new Characters();
 
-            Helper.CustomAreEqual("XX", t.Execute("XX"));
-            Helper.CustomAreEqual("XX", t.Execute("XX"));
-            Helper.CustomAreEqual("XX", t.Execute("XX"));
-            Helper.CustomAreEqual("XX", t.Execute("XX"));
-            Helper.CustomAreEqual("XX", t.Execute("XX"));
-        }
-        [TestMethod]
-        public void _Reverser()
+        [Theory]
+        [InlineData("XX", "XX")]
+        public void _Reverser(string input, string expectedResult)
         {
-            var t = new Reverser();
+            var tested = new Reverser();
 
-            Helper.CustomAreEqual("XX", t.Execute("XX"));
-            Helper.CustomAreEqual("XX", t.Execute("XX"));
-            Helper.CustomAreEqual("XX", t.Execute("XX"));
-            Helper.CustomAreEqual("XX", t.Execute("XX"));
-            Helper.CustomAreEqual("XX", t.Execute("XX"));
+            Assert.Equal(expectedResult, tested.Execute(input)[0]);
         }
     }
 }
